500b8f9fca8a6b7109b3c3629d83e7f433316c89
unknown <guyfanilotiana@gmail.com>
2021-04-09 19:01:34 -0400

    FGR :: Ajout affichageGraphe

--- src/partie1.cpp
+++ src/partie1.cpp
@@ -1,15 +1,51 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>    
 #include "../include/partie1.h"
-
+#include <sstream>
+#include <list>
+#include <vector>
 using namespace std;
+#define TAILLE_MAX 1000
 
+vector<vector<string>> listeContacts;
 void creerGrapheExposition(string nomFichierIndividus, string nomFichierContacts){
     cout << "Option a\n";
+    ifstream fluxContacts("./"+ nomFichierContacts +".txt");     
+    
+    if(fluxContacts)  //On teste si tout est OK
+    {   
+        cout << "Fichier "+nomFichierContacts+".txt ouvert" << endl;
+        string ligne;
+        while(getline(fluxContacts, ligne)) //Tant qu'on n'est pas à la fin, on lit
+        {      
+            string arr[3];
+            int i = 0;
+            stringstream ssin(ligne);
+            vector<string> line ;
+            while (ssin.good() && i < 3){
+                ssin >> arr[i];
+                line.push_back(arr[i]);
+                ++i;
+                
+            }
+            listeContacts.push_back(line);
+            
+        }
+         
+    }
+    else
+    {
+        cout << "ERREUR: Impossible d'ouvrir le fichier." << endl;
+    }
+
 }
 
 void afficherGrapheExposition(){
-    cout << "Option b\n";
+     cout << "Option b\n";
+    for(int h = 0 ; h<listeContacts.capacity(); h++){            
+        cout << "(" +listeContacts.at(h).at(0)+" " + listeContacts.at(h).at(2) + " (" +listeContacts.at(h).at(1)+"))"<< endl;            
+    }  
 }
 
 void identifierExposition(string origine, string destination){}
